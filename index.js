1f561c9f5cc94d4d870a6b9261da437ab8d5de89
mohamedelgallad-510 <mohamed.elgallad9@gmail.com>
2026-01-19 22:58:03 +0300

    Added like functionality

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const posts = [
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
         likes: 21,
+        isLiked: false,
         uuid: uuidv4()
     },
     {
@@ -19,6 +20,7 @@ const posts = [
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4,
+        isLiked: false,
         uuid: uuidv4()
     },
         {
@@ -29,6 +31,7 @@ const posts = [
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152,
+        isLiked: false,
         uuid: uuidv4()
     }
 ]
@@ -50,9 +53,9 @@ function renderPosts() {
             <img class="post-image" src="${post.post}" alt="Post by ${post.username}.">
             <div class="post-details">
                 <div class="post-icons">
-                    <i class="fa-regular fa-heart icon"></i>
-                    <i class="fa-regular fa-comment icon"></i>
-                    <i class="fa-regular fa-paper-plane icon"></i>
+                    <i class="fa-regular fa-heart icon" data-like='${post.uuid}'></i>
+                    <i class="fa-regular fa-comment icon" data-comment='${post.uuid}'></i>
+                    <i class="fa-regular fa-paper-plane icon" data-share='${post.uuid}'></i>
                 </div>
                 <div class="post-likes-comments">
                     <p class="no-margin"><strong>${post.likes} likes</strong></p>
@@ -75,3 +78,25 @@ renderPosts()
         renderPosts();
     }
 }); */
+
+document.addEventListener("click", function(e) {
+    if (e.target.dataset.like) {
+        handleLikeClick(e.target.dataset.like);
+    }});
+
+function handleLikeClick(postUuid) {
+    const post = posts.find(post => post.uuid === postUuid);
+    if (post) {
+        if (!post.isLiked) {
+            post.likes += 1;
+            post.isLiked = true;
+            document.querySelector(`i[data-like='${postUuid}']`).classList.remove("fa-regular");
+            document.querySelector(`i[data-like='${postUuid}']`).classList.add("fa-solid");
+        } else {
+            post.likes -= 1;
+            post.isLiked = false;
+            document.querySelector(`i[data-like='${postUuid}']`).classList.remove("fa-solid");
+            document.querySelector(`i[data-like='${postUuid}']`).classList.add("fa-regular");
+        }
+}
+renderPosts();}
